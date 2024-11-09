115aab88a9694d1aaa56faaae5a586f8310487f1
Marcielo Abalos <eloicram18@gmail.com>
2024-11-09 23:08:42 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> {{title}} </h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: "Reaction Timer Game"
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -20,7 +23,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
