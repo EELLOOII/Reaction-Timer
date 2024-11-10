1e17cc6ecfae427233573266ed359341fae48470
Marcielo Abalos <eloicram18@gmail.com>
2024-11-11 00:30:22 +0800

    Emitting custom events with data

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,16 @@
   <div>
     <h1>{{ title }}</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
-    <Block v-if="isPlaying" :delay="delay"/>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+    <p v-if="showResults">Reaction time: {{ score }} ms</p>
+    <!-- <Results/> -->
   </div>   
 </template>
 
 <script>
 
 import Block from "./components/Block.vue"
+// import Results from "./components/Results.vue"
 
 export default {
   name: "App",
@@ -19,14 +22,22 @@ export default {
       title: "Reaction Timer Game",
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults : false
     };
   },
   methods: {
     start() {
+      this.showResults = false
       this.isPlaying = true
       this.delay = 2000 + Math.random() * 5000
     },
-  },
+    endGame(reactionTime) {
+      this.showResults = true
+      this.score = reactionTime 
+      this.isPlaying = false
+    }
+  }
 };
 </script>
 
