be2e9403ec2987de1935b45baebc808a20f8cbed
Marcielo Abalos <eloicram18@gmail.com>
2024-11-11 00:43:08 +0800

    Added a Results component and mount it in the Root component

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
     <h1>{{ title }}</h1>
     <button @click="start" :disabled="isPlaying">Play</button>
     <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-    <p v-if="showResults">Reaction time: {{ score }} ms</p>
-    <!-- <Results/> -->
+    <!-- <p v-if="showResults">Reaction time: {{ score }} ms</p> -->
+    <Results v-if="showResults" :score="score"/>
   </div>   
 </template>
 
 <script>
 
 import Block from "./components/Block.vue"
-// import Results from "./components/Results.vue"
+import Results from "./components/Results.vue"
 
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Results },
 
   data() {
     return {
